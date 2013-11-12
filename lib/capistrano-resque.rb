6b51ae572078c3ff7e0bcd679104d93dc1b76bf2
Andrei Varanovich <dotnetby@gmail.com>
2013-11-12 10:25:00 +0100

    Update capistrano-resque.rb

--- lib/capistrano-resque.rb
+++ lib/capistrano-resque.rb
@@ -1,6 +1,6 @@
 require "capistrano-resque/version"
 
-if defined?(Capistrano::Version) && Gem::Version.new(Capistrano::Version).release >= Gem::Version.new('3.0.0')
+if Gem::Version.new(Capistrano::Version).release >= Gem::Version.new('3.0.0')
   load File.expand_path("../capistrano-resque/tasks/capistrano-resque.rake", __FILE__)
 else
   require "capistrano-resque/capistrano_integration"
